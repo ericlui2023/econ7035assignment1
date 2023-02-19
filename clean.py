4ded7f6200afd25371f04e340ea4b340705d5869
elui <21458650@life.hkbu.edu.hk>
2023-02-19 19:16:45 +0800

    updates for output info

--- clean.py
+++ clean.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
     cleaned = clean(args.input1, args.input2)
     cleaned.to_csv(args.output, index=False)
     
-
+print('The shape of the output file:' + str(cleaned.shape))
       
     
     
